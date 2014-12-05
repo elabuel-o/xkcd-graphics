d50f9f892fd510a00988c273133bbe23f240e66b
elabuel-o <a_enriquez@live.com.mx>
2014-12-04 23:19:16 -0600

    first commit

---
+++ xkcdGraphs.R
@@ -0,0 +1,70 @@
+
+## Loading the packages
+library(xkcd)
+library(ggplot2)
+
+## Loading the fonts
+library(extrafont)
+fonts()
+loadfonts(device = "win")
+
+## Let us make our first graphic
+data(mtcars) ## the dataset (cars!)
+head(mtcars)
+
+names(mtcars)[1] <- "MPG"
+names(mtcars)[6] <- "WT"
+
+## A preparatory graphic! (to make sure we've the fonts)
+ggplot() + geom_point(aes(x = MPG, y = WT), data = mtcars) +
+        theme_xkcd()
+
+xrange <- range(mtcars$MPG)
+yrange <- range(mtcars$WT)
+
+## The graphic itself
+g <- ggplot() + geom_point(aes(x = MPG, y = WT), data = mtcars) + 
+        labs(list(title = "CARS", x = "MILES PER GALLON", 
+                  y = "WEIGHT IN POUNDS")) + 
+        xkcdaxis(xrange, yrange)
+
+## Printing in PDF
+library(showtext)
+library(sysfonts)
+
+font.add("xkcd", "xkcd.ttf") ## Loading the fonts
+
+## PDF device
+pdf("xkcdGraphic4.pdf")
+showtext.begin()
+print(g)
+showtext.end()
+dev.off()
+
+## Now a barchart
+data <- data.frame(year = c(2007:2011),
+                   number = c(470, 998, 686, 1783, 251))
+
+data$xmin <- data$year - 0.1
+data$xmax <- data$year + 0.1
+data$ymin <- 0
+data$ymax <- data$number
+
+xrange <- range(min(data$xmin) - 0.1, max(data$xmax) + 0.1)
+yrange <- range(min(data$ymin), max(data$ymax))
+
+mapping <- aes(xmin = xmin, ymin = ymin, 
+               xmax = xmax, ymax = ymax)
+
+g2 <- ggplot() + xkcdrect(mapping, data) +
+        xkcdaxis(xrange, yrange) +
+        xlab("YEAR") + ylab("NUMBER OF WORKERS")
+
+
+## PDF device
+pdf("xkcdGraphic5.pdf")
+showtext.begin()
+print(g2)
+showtext.end()
+dev.off()
+
